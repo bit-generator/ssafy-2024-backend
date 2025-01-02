c33a0117876a54a453871330a1750ea83d15f270
THK <k4mat18@gmail.com>
2025-01-02 13:53:39 +0900

    add files

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from langchain.chains import RetrievalQA
 from langchain_pinecone import PineconeVectorStore
 from langchain_upstage import ChatUpstage
 from langchain_upstage import UpstageEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser #문자열(text, string)만 나오게 하는 출력 파서
 from pinecone import Pinecone, ServerlessSpec
 from pydantic import BaseModel
 
@@ -19,7 +21,7 @@ embedding_upstage = UpstageEmbeddings(model="embedding-query")
 
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 pc = Pinecone(api_key=pinecone_api_key)
-index_name = "finance"
+index_name = "eco"
 
 # create new index
 if index_name not in pc.list_indexes().names():
@@ -68,19 +70,38 @@ class MessageRequest(BaseModel):
 
 @app.post("/chat")
 async def chat_endpoint(req: MessageRequest):
+    clean_msg = req.message.strip()
     qa = RetrievalQA.from_chain_type(llm=chat_upstage,
                                      chain_type="stuff",
                                      retriever=pinecone_retriever,
                                      return_source_documents=True)
 
-    result = qa(req.message)
-    return {"reply": result['result']}
+    result = qa(clean_msg)
+
+    llm = ChatUpstage()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "너는 금융용어 및 금융상품 설명 챗봇이 올바르게 답변하는지를 필터링하는 봇이야." \
+             "다음과 같은 요구사항에 따라 출력해야 돼." \
+                 "금융상품 관련 내용: 가장 적합한 내용을 찾아 답변을 제시하고, 수치가 있을 경우 수치를 함께 제시. 수치는 가장 중요한 값을 우선적으로 제시하고, 다른 수치가 있다면 함께 제시. 만약 해당 금융상품에 대한 내용이 문서에 없다면 모른다고 답변해" \
+                    "음식이나 날씨, 스포츠, 게임 등 금융과 관련되지 않은 내용: 금융 관련 챗봇이기 때문에 모르겠다거나 설명해드릴 수 없다고 답할 것."),
+
+            # User Query
+            ("human", result['result']),
+        ]
+    )
+
+    chain = prompt | llm | StrOutputParser() # with output parser
+
+    #invoke the chain
+    c_result = chain.invoke({})
+    return {"reply": c_result}
 
 
 # @app.post("/assistant")
 # async def assistant_endpoint(req: AssistantRequest):
 #     assistant = await openai.beta.assistants.retrieve("asst_tc4AhtsAjNJnRtpJmy1gjJOE")
-#
+
 #     if req.thread_id:
 #         # We have an existing thread, append user message
 #         await openai.beta.threads.messages.create(
@@ -93,22 +114,22 @@ async def chat_endpoint(req: MessageRequest):
 #             messages=[{"role": "user", "content": req.message}]
 #         )
 #         thread_id = thread.id
-#
+
 #     # Run and wait until complete
 #     await openai.beta.threads.runs.create_and_poll(
 #         thread_id=thread_id, assistant_id=assistant.id
 #     )
-#
+
 #     # Now retrieve messages for this thread
 #     # messages.list returns an async iterator, so let's gather them into a list
 #     all_messages = [
 #         m async for m in openai.beta.threads.messages.list(thread_id=thread_id)
 #     ]
 #     print(all_messages)
-#
+
 #     # The assistant's reply should be the last message with role=assistant
 #     assistant_reply = all_messages[0].content[0].text.value
-#
+
 #     return {"reply": assistant_reply, "thread_id": thread_id}
 
 
