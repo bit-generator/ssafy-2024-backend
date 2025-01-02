feb210870f1264bf472421ec7cb80b3e9ac9d868
THK <k4mat18@gmail.com>
2025-01-02 17:14:28 +0900

    add file

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ embedding_upstage = UpstageEmbeddings(model="embedding-query")
 
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 pc = Pinecone(api_key=pinecone_api_key)
-index_name = "eco"
+index_name = "eco3"
 
 # create new index
 if index_name not in pc.list_indexes().names():
@@ -70,46 +70,50 @@ class MessageRequest(BaseModel):
 
 @app.post("/chat")
 async def chat_endpoint(req: MessageRequest):
-    clean_msg = req.message.strip()
+    clean_msg = req.message.strip()     # 사용자 질문
     qa = RetrievalQA.from_chain_type(llm=chat_upstage,
                                      chain_type="stuff",
                                      retriever=pinecone_retriever,
                                      return_source_documents=True)
 
-    result = qa(clean_msg)
+    result = qa(clean_msg)      # 챗봇 답변
+    resultMsg = "Q: " + clean_msg + "\n" + "A: " + result['result']
 
     llm = ChatUpstage()
     prompt = ChatPromptTemplate.from_messages(
         [
             ("system", "너는 금융용어 및 금융상품 설명 챗봇이 올바르게 답변하는지를 필터링하는 봇이야." \
-             "다음과 같은 요구사항에 따라 출력해야 돼." \
-                 "금융상품 관련 내용: 가장 적합한 내용을 찾아 답변을 제시하고, 수치가 있을 경우 수치를 함께 제시. 수치는 가장 중요한 값을 우선적으로 제시하고, 다른 수치가 있다면 함께 제시. 만약 해당 금융상품에 대한 내용이 문서에 없다면 모른다고 답변해" \
-                    "음식, 날씨, 스포츠, 게임, 컴퓨터, 언어, 음악, 드라마, 애니메이션, 영화 등 모든 금융과 관련되지 않은 내용: 금융 관련 챗봇이기 때문에 모르겠다거나 설명해드릴 수 없다고 답할 것." \
-                        "너의 존재에 대해 묻는다면 금융용어 및 금융상품을 알려주는 챗봇이라고 답할 것."
+             "내용은 Q: (질문 내용)\\nA: (답변 내용) 과 같은 형식으로 들어오는데, 이것을 다음과 같은 요구사항에 따라 출력해야 돼." \
+                 "(질문 내용)이 금융상품 관련 내용이면 무조건 (답변 내용)을 출력해야 돼." \
+                    "(질문 내용)이 음식, 날씨, 스포츠, 게임, 컴퓨터, 언어, 음악, 드라마, 애니메이션, 영화 등 금융과 관련되지 않은 내용이라면 금융 관련 챗봇이기 때문에 모르겠다거나 설명해드릴 수 없다고 출력해야 돼." \
+                        "(답변 내용)에 (질문 내용)에서 묻지 않은 회사의 내용이 포함되어 있다면, (답변 내용)에서 그 부분은 제외하고 다듬어서 출력해야 돼." \
+                        "(질문 내용)에서 \"국민은행\"에 대해 묻는다면 \"KB국민은행\"으로 취급해 줘."
                         ),
 
             # few-shot prompting
-            ("human", "감가상각이 뭔지 모르겠는데 알려줘."),  # human request
-            ("ai", "감가상각은 ~ "),      # LLM response
-            ("human", "C++과 python 중 어느 게 AI 개발에 더 적합하지?"),
-            ("ai",  "프로그래밍 언어는 제 전문이 아니므로 답변해드릴 수 없습니다."),
-            ("human", "로맨스 드라마 추천 좀 해줘."),
-            ("ai",  "드라마는 제 전문이 아니므로 답변해드릴 수 없습니다."),
-            ("human", "로맨스 드라마 추천 좀 해줘."),
-            ("ai",  "드라마는 제 전문이 아니므로 답변해드릴 수 없습니다."),
-            ("human", "국민은행의 금융상품에는 어떤 것이 있는지 설명해줘."),
-            ("ai",  "국민은행에는 ~"),
-            ("human", "오늘 금융상담 중 \"만기\"라는 용어가 나왔는데 무슨 뜻인지 모르겠다. 알려줄 수 있어?"),
-            ("ai",  "만기는 ~"),
-            ("human", "리그 오브 레전드에 대해 설명해줘."),
-            ("ai",  "게임은 제 전문이 아니므로 답변해드릴 수 없습니다."),
-            ("human", "안녕하세요는 일본어로 뭐라고 해?"),
-            ("ai",  "언어는 제 전문이 아니므로 답변해드릴 수 없습니다."),
-            ("human", "오늘 저녁은 뭐가 좋을지 추천해줄 수 있어?"),
+            ("human", "Q: 감가상각이 뭐지?\\nA: 감가상각은 ~ 입니다."),  # human request
+            ("ai", "감가상각은 ~ 입니다."),      # LLM response
+            ("human", "Q: 오늘 점심 추천 좀 해줘.\\nA: 오늘 점심으로는 ~을 추천드립니다."),
             ("ai",  "음식은 제 전문이 아니므로 답변해드릴 수 없습니다."),
+            ("human", "Q: C++에서 클래스가 뭐지?\\nA: C++에서 클래스는 ~을 의미합니다."),
+            ("ai",  "프로그래밍은 제 전문이 아니므로 답변해드릴 수 없습니다."),
+            ("human", "Q: 국민은행의 금융상품에는 뭐가 있는지 궁금해.\\nA: 국민은행의 금융상품에는 'KB국민첫재테크적금', '청년 처음적금', '신한 땡겨요 적금', 'KB스타적금Ⅱ' 등이 있습니다."),
+            ("ai",  "국민은행의 금융상품에는 'KB국민첫재테크적금', '청년 처음적금', 'KB스타적금Ⅱ' 등이 있습니다."),
+            ("human", "Q: 오늘 금융상담 중 \"만기\"라는 용어가 나왔는데 무슨 뜻인지 모르겠다. 알려줄 수 있어?\\nA: 금융에서 만기란 ~을 말하며, ~"),
+            ("ai",  "금융에서 만기란 ~을 말하며, ~"),
+            ("human", "Q: 리그 오브 레전드에 대해 설명해줘.\\nA: 리그 오브 레전드는 라이엇 게임즈에서 개발한 ~"),
+            ("ai",  "게임은 제 전문이 아니므로 답변해드릴 수 없습니다."),
+            ("human", "Q: 안녕하세요는 일본어로 뭐라고 해?\\nA: \"안녕하세요\"는 일본어로 \"곤니치와\"라고 합니다."),
+            ("ai",  "언어는 제 전문이 아니므로 답변해드릴 수 없습니다."),
+            ("human", "Q: 국민은행의 ~ 상품의 금리는 얼마인지 궁금해.\\nA: ~ 상품의 금리는 다음과 같습니다. ~"),
+            ("ai",  "~ 상품의 금리는 다음과 같습니다. ~"),
+            ("human", "Q: 신한은행에는 어떤 금융상품이 있는지 설명해 줘.\\nA: 신한은행의 금융상품에는 'KB국민첫재테크적금', '청년 처음적금', '신한 땡겨요 적금', '신한 쏠만해 적금' 등이 있습니다."),
+            ("ai",  "신한은행의 금융상품에는 '신한 땡겨요 적금', '신한 쏠만해 적금' 등이 있습니다."),
+            ("human", "Q: 우리은행의 금융상품에는 어떤 게 있는지 알려줘.\\nA: 우리은행의 금융상품에는 '신한 땡겨요 적금'과 '외화보통예금'이 있습니다."),
+            ("ai",  "우리은행의 금융상품에는 '외화보통예금'이 있습니다."),
 
             # User Query
-            ("human", result['result']),
+            ("human", resultMsg),
         ]
     )
 
@@ -118,42 +122,6 @@ async def chat_endpoint(req: MessageRequest):
     #invoke the chain
     c_result = chain.invoke({})
     return {"reply": c_result}
-
-
-# @app.post("/assistant")
-# async def assistant_endpoint(req: AssistantRequest):
-#     assistant = await openai.beta.assistants.retrieve("asst_tc4AhtsAjNJnRtpJmy1gjJOE")
-
-#     if req.thread_id:
-#         # We have an existing thread, append user message
-#         await openai.beta.threads.messages.create(
-#             thread_id=req.thread_id, role="user", content=req.message
-#         )
-#         thread_id = req.thread_id
-#     else:
-#         # Create a new thread with user message
-#         thread = await openai.beta.threads.create(
-#             messages=[{"role": "user", "content": req.message}]
-#         )
-#         thread_id = thread.id
-
-#     # Run and wait until complete
-#     await openai.beta.threads.runs.create_and_poll(
-#         thread_id=thread_id, assistant_id=assistant.id
-#     )
-
-#     # Now retrieve messages for this thread
-#     # messages.list returns an async iterator, so let's gather them into a list
-#     all_messages = [
-#         m async for m in openai.beta.threads.messages.list(thread_id=thread_id)
-#     ]
-#     print(all_messages)
-
-#     # The assistant's reply should be the last message with role=assistant
-#     assistant_reply = all_messages[0].content[0].text.value
-
-#     return {"reply": assistant_reply, "thread_id": thread_id}
-
 
 @app.get("/health")
 @app.get("/")
